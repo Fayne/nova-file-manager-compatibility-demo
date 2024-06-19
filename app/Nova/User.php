d13cda8d29610f73aa5ff923abb8c5e61426fd1b
Fayne Guo <fayne.guo@gmail.com>
2024-06-19 13:03:03 +0800

    :+1: Reproduce the issue

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Nova;
 
+use App\Nova\Repeater\UserInfoItem;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
 use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Repeater;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -62,6 +64,10 @@ class User extends Resource
                 ->onlyOnForms()
                 ->creationRules('required', Rules\Password::defaults())
                 ->updateRules('nullable', Rules\Password::defaults()),
+            Repeater::make('Profile')
+                ->repeatables([
+                    UserInfoItem::make(),
+                ]),
         ];
     }
 
